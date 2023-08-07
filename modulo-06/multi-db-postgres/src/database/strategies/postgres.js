b6db85e3f85b474fd53bdfc6cad5498764d40533
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2023-08-07 18:05:26 -0300

    feat/adding-module-6-crud-with-postgres

--- modulo-06/multi-db-postgres/src/database/strategies/postgres.js
+++ modulo-06/multi-db-postgres/src/database/strategies/postgres.js
@@ -1,24 +1,77 @@
+const Sequelize = require('sequelize');
 const ICrud = require('./interfaces/ICrud');
 
 class Postgres extends ICrud {
     constructor() {
-        super()
+        super();
+        this._driver = null;
+        this._herois = null;
     }
 
-    create(item) {
-        console.log("O item foi salvo no Postgres")
+    async create(item) {
+        return await this._herois.create(item);
     }
 
-    read(query) {
-        console.log("")
+    async read(item = {}) {
+        return this._herois.findAll({ where: item, raw: true });
     }
 
-    update(id, item) {
-        console.log("")
+    async update(id, item) {
+        return await this._herois.update(item, { where: { id: id } });
     }
 
-    delete(id) {
-        console.log("")
+    async delete(id) {
+        const query = id ? { id } : {};
+        return await this._herois.destroy({ where: query });
+    }
+
+    async isConnected() {
+        try {
+            await this._driver.authenticate();
+            return true;
+        } catch (error) {
+            console.error('Fail!', error);
+            return false;
+        }
+    }
+
+    async defineModel() {
+        this._herois = this._driver.define('herois', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            nome: {
+                type: Sequelize.STRING,
+                required: true
+            },
+            poder: {
+                type: Sequelize.STRING,
+                required: true
+            }
+        }, {
+            tableName: 'TB_HEROIS',
+            freezeTableName: false,
+            timestamps: false
+        });
+        await this._herois.sync();
+    }
+
+    async connect() {
+        this._driver = new Sequelize(
+            'heroes',
+            'koroch',
+            '123456',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
+        await this.defineModel()
     }
 }
 
