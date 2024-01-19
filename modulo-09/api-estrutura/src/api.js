cc60e812d30880c94dcce9f01526509ec1f4b7bb
Jonatan Koroch de Aguiar <Jonatan Koroch@compumate.local>
2024-01-19 13:12:13 -0300

    feat/module09-test-app

--- modulo-09/api-estrutura/src/api.js
+++ modulo-09/api-estrutura/src/api.js
@@ -2,23 +2,22 @@ const Hapi = require('hapi');
 const Context = require('./database/strategies/base/contextStrategy');
 const MongoDB = require('./database/strategies/mongodb/mongodb');
 const HeroiSchema = require('./database/strategies/mongodb/schemas/heroisSchema');
+const HeroRoutes = require('./routes/heroRoutes');
 
 const app = new Hapi.Server({
     port: 5000
 })
+
+function mapRoutes(instance, methods) {
+    return methods.map(method => instance[method]())
+}
 
 async function main() {
     const connection = MongoDB.connect();
     const context = new Context(new MongoDB(connection, HeroiSchema));
 
     app.route([
-        {
-            path: '/herois',
-            method: 'GET',
-            handler: (request, response) => {
-                return context.read()
-            }
-        }
+        ...mapRoutes(new HeroRoutes(context), HeroRoutes.methods())
     ])
 
     await app.start()
