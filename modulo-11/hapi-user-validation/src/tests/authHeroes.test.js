4dbc343862d654566692d805839179867f34bddd
Jonatan Koroch de Aguiar <Jonatan Koroch@compumate.local>
2024-01-30 13:12:07 -0300

    fix/module11-hapi-validation-with-bcrypt

--- modulo-11/hapi-user-validation/src/tests/authHeroes.test.js
+++ modulo-11/hapi-user-validation/src/tests/authHeroes.test.js
@@ -1,19 +1,31 @@
 const assert = require('assert');
 const api = require('../api');
+const Context = require('../database/strategies/base/contextStrategy');
+const Postgres = require('../database/strategies/postgres/postgres');
+const UsuarioSchema = require('../database/strategies/postgres/schemas/usuarioSchema');
+
+const USER = {
+    username: 'joaoninguem',
+    password: '123456'
+}
+
+const USER_DB = {
+    ...USER,
+    password: ''
+}
 
 describe('Auth test suite', function () {
     this.beforeAll(async () => {
         app = await api;
+        const connectionPostgres = await Postgres.connect();
+        const model = await Postgres.defineModel(connectionPostgres, UsuarioSchema);
     })
 
     it('deve obter um token', async () => {
         const result = await app.inject({
             method: 'POST',
             url: '/login',
-            payload: {
-                username: 'joaoninguem',
-                password: '123456'
-            }
+            payload: USER
         })
 
         const statusCode = result.statusCode;
@@ -22,4 +34,5 @@ describe('Auth test suite', function () {
         assert.deepEqual(statusCode, 200);
         assert.ok(dados.token.length > 10);
     })
+
 })
